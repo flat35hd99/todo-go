830ba8cda5f23d90898125ea87620f23de632417
flat35hd99 <flat35hd99@gmail.com>
2022-07-30 13:33:06 +0000

    Add e2e test

--- backend/e2e_test.go
+++ backend/e2e_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 
 	"github.com/steinfletcher/apitest"
+	"github.com/stretchr/testify/assert"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -45,11 +47,18 @@ func TestE2E(t *testing.T) {
 			End()
 	})
 
-	t.Run("Normal: Get users list", func(t *testing.T) {
+	t.Run("Normal: Get users", func(t *testing.T) {
 		t.Parallel()
 
 		db, err := newMockDB(t)
 		if err != nil {
+			t.Error(err)
+		}
+		user := User{
+			Name: "test",
+			Age:  30,
+		}
+		if err = db.Create(&user).Error; err != nil {
 			t.Error(err)
 		}
 		apitest.New().
@@ -58,5 +67,40 @@ func TestE2E(t *testing.T) {
 			Expect(t).
 			Status(http.StatusOK).
 			End()
+	})
+
+	t.Run("Normal: Create users", func(t *testing.T) {
+		t.Parallel()
+
+		db, err := newMockDB(t)
+		if err != nil {
+			t.Error(err)
+		}
+		apitest.New().
+			Handler(newApp(db)).
+			Post("/users").
+			ContentType("application/json").
+			Body(`{"name":"test","age":30}`).
+			Expect(t).
+			Status(http.StatusOK).
+			End()
+
+		type user struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+			Age  int    `json:"age"`
+		}
+		var u user
+		assert.NoError(t, db.First(&u).Error)
+		assert.Equal(t, u.Name, "test")
+		assert.Equal(t, u.Age, 30)
+
+		u_db := User{}
+		assert.NoError(t, db.First(&u_db).Error)
+		assert.Equal(t, u_db.Name, "test")
+		assert.Equal(t, u_db.Age, uint(30))
+		assert.NotEmpty(t, u_db.ID)
+		assert.NotEmpty(t, u_db.CreatedAt)
+		assert.NotEmpty(t, u_db.UpdatedAt)
 	})
 }
