892b2cb3a72c74e5f4bcc3f3cf49fa5f978fd386
flat35hd99 <flat35hd99@gmail.com>
2022-08-07 08:01:29 +0000

    Add lambda function endpoint url

--- lambda.tf
+++ lambda.tf
@@ -4,14 +4,23 @@ locals {
 
 resource "aws_lambda_function" "server" {
   function_name = local.lambda_function_name
-  role          = aws_iam_role.for_lambda
+  role          = aws_iam_role.for_lambda.arn
 
-  runtime = "go1.x"
+  runtime          = "go1.x"
+  filename         = "function_payload.zip"
+  handler          = "app"
+  source_code_hash = filebase64sha256("function_payload.zip")
+}
 
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_log,
-    aws_cloudwatch_log_group.lambda_log
-  ]
+resource "aws_lambda_function_url" "endpoint" {
+  function_name      = aws_lambda_function.server.function_name
+  authorization_type = "NONE"
+  cors {
+    allow_headers = ["*"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
+    max_age       = 60
+  }
 }
 
 resource "aws_iam_role" "for_lambda" {
@@ -34,7 +43,7 @@ EOF
 }
 
 resource "aws_cloudwatch_log_group" "lambda_log" {
-  name              = "/aws/lambda/${locals.lambda_function_name}"
+  name              = "/aws/lambda/${local.lambda_function_name}"
   retention_in_days = 14
 }
 
@@ -62,6 +71,7 @@ EOF
 }
 
 resource "aws_iam_policy_attachment" "lambda_log" {
-  roles      = aws_iam_role.for_lambda.name
+  name       = "lambda_log"
+  roles      = [aws_iam_role.for_lambda.name]
   policy_arn = aws_iam_policy.lambda_logging.arn
 }
