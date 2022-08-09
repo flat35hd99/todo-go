902781b21b6e2d484975efb06d53574498a787ec
flat35hd99 <flat35hd99@gmail.com>
2022-08-09 10:38:01 +0000

    Add some comments

--- lambda.tf
+++ lambda.tf
@@ -77,6 +77,7 @@ resource "aws_iam_policy_attachment" "lambda_log" {
   policy_arn = aws_iam_policy.lambda_logging.arn
 }
 
+// Build lambda function executable binary
 resource "null_resource" "buildstep" {
   triggers = {
     "hash" = join("", [for f in fileset(path.module, "/backend/**/*.go") : filebase64sha256(f)])
@@ -87,6 +88,7 @@ resource "null_resource" "buildstep" {
   }
 }
 
+// Archive binary file to upload
 data "archive_file" "archive_binary" {
   source_file = "${path.module}/backend/cmd/lambda/${local.lambda_binary_filename}"
   output_path = "${path.module}/function_payload.zip"
